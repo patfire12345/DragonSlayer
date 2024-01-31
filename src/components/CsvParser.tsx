c94e699dc662be261e25a67fb6a8e11eab85c800
Patrick <phuan097@uottawa.ca>
2024-01-31 12:07:00 -0500

    <feature> Implemented TextParser to extract csv within text

--- src/components/CsvParser.tsx
+++ src/components/CsvParser.tsx
@@ -7,8 +7,41 @@ export default function CsvParser() {
   const { readString } = usePapaParse();
   const [tableData, setTableData] = useState<any[]>([]);
 
+  const parseText = (text: string, delimitter: string, tolerance: number) => {
+    const splitText = text.split("\n")
+    const newSplitText = []
+
+    const headerText = splitText[0].split(" ")
+    const csvHeaderText = headerText[headerText.length-1]
+
+    newSplitText.push(csvHeaderText)
+
+    let delimitterCount = 0;
+
+    for (let i = 0; i < csvHeaderText.length; i++) {
+      if (csvHeaderText[i] === delimitter) {
+        delimitterCount++;
+      }
+    }
+
+    for (let i = 1; i < splitText.length; i++) {
+      let innerDelimiterCount = 0
+      for (let j = 0; j < splitText[i].length; j++) {
+        if (splitText[i][j] === delimitter) {
+          innerDelimiterCount++;
+        }
+      }
+
+      if (innerDelimiterCount >= delimitterCount - tolerance && innerDelimiterCount <= delimitterCount + tolerance) {
+        newSplitText.push(splitText[i])
+      }
+    }
+
+    return newSplitText.join("\n")
+  }
+
   const handleReadString = () => {
-    readString(csvString, {
+    readString(parseText(csvString,",",1), {
       worker: true,
       complete: (results) => {
         setTableData(results.data);
