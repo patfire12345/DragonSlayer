61f1aa3f5d9d2c5fc8a6edac8e50e82ad5731259
Patrick <phuan097@uottawa.ca>
2024-02-01 11:40:10 -0500

    <feature> Multiple Tables added

--- src/components/CsvParser.tsx
+++ src/components/CsvParser.tsx
@@ -2,64 +2,96 @@ import { Button, Container, Stack, Textarea } from "@chakra-ui/react";
 
 import DataTable from "./DataTable";
 import { usePapaParse } from "react-papaparse";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function CsvParser() {
   const [userInput, setUserInput] = useState<string>("");
   const { readString } = usePapaParse();
   const [tableData, setTableData] = useState<any[]>([]);
 
-  const parseText = (text: string, delimitter: string, tolerance: number) => {
+  useEffect(() => {
+    console.log(tableData)
+  },[tableData])
+
+  const parseText = (text: string, delimitter: string, tolerance: number, numberOfTables: number) => {
     const splitText = text.split("\n")
-    const newSplitText = []
+    const tableList = []
 
-    let delimitterCount = 0;
-    let headerRowFound = false;
+    let tableCounter = 0;
+    let i = 0;
 
-    for (let i = 0; i < splitText.length; i++) {
-      if (!headerRowFound) {
-        const headerText = splitText[i].split(" ")
-        const csvHeaderText = headerText[headerText.length-1]
+    while (tableCounter < numberOfTables) {
+      let delimitterCount = 0;
+      let headerRowFound = false;
+      const newSplitText = []
 
-        if (csvHeaderText.includes(delimitter)) {
-          newSplitText.push(csvHeaderText)
-          headerRowFound = true
-  
-          for (let j = 0; j < csvHeaderText.length; j++) {
-            if (csvHeaderText[j] === delimitter) {
-              delimitterCount++;
+      while (i < splitText.length) {
+        if (!headerRowFound) {
+          const headerText = splitText[i].split(" ")
+          const csvHeaderText = headerText[headerText.length-1]
+
+          if (csvHeaderText.includes(delimitter)) {
+            newSplitText.push(csvHeaderText)
+            headerRowFound = true
+    
+            for (let j = 0; j < csvHeaderText.length; j++) {
+              if (csvHeaderText[j] === delimitter) {
+                delimitterCount++;
+              }
             }
           }
 
-          
+          i++;
+          continue
         }
 
-        continue
-      }
-
-      let innerDelimiterCount = 0
-      for (let j = 0; j < splitText[i].length; j++) {
-        if (splitText[i][j] === delimitter) {
-          innerDelimiterCount++;
+        let innerDelimiterCount = 0
+        for (let j = 0; j < splitText[i].length; j++) {
+          if (splitText[i][j] === delimitter) {
+            innerDelimiterCount++;
+          }
         }
+
+        if (innerDelimiterCount >= delimitterCount - tolerance && innerDelimiterCount <= delimitterCount + tolerance) {
+          newSplitText.push(splitText[i])
+        }
+
+        else {
+          break
+        }
+
+        i++;
       }
 
-      if (innerDelimiterCount >= delimitterCount - tolerance && innerDelimiterCount <= delimitterCount + tolerance) {
-        newSplitText.push(splitText[i])
-      }
+      tableList.push(newSplitText.join("\n"))
+      tableCounter++;
     }
 
-    return newSplitText.join("\n")
+    return tableList
   }
 
   const handleReadString = () => {
-    readString(parseText(userInput,",",1), {
-      worker: true,
-      complete: (results) => {
-        setTableData(results.data);
-      },
-    });
+    const tableList = parseText(userInput,"\t",1,2)
+
+    for (let i = 0; i < tableList.length; i++) {
+      readString(tableList[i], {
+        worker: true,
+        complete: (results) => {
+          setTableData([...tableData,results.data]);
+        },
+      });
+    }
   };
+
+  const displayTables = () => {
+    return (
+      tableData.map((data) => {
+        return (
+          <DataTable data={data} />
+        )
+      })
+    )
+  }
 
   return (
     <Container centerContent>
@@ -71,7 +103,7 @@ export default function CsvParser() {
           rows={10}
         />
         <Button onClick={() => handleReadString()}>Parse CSV</Button>
-        <DataTable data={tableData} />
+        {tableData.length !== 0 ? displayTables() : null}
       </Stack>
     </Container>
   );
