f97d697c5b13edf50f565c3faba8e033afa467dd
Patrick <phuan097@uottawa.ca>
2024-02-06 12:12:28 -0500

    <feature> Implemented Delimiter Dropdown

--- src/components/CsvParser.tsx
+++ src/components/CsvParser.tsx
@@ -1,19 +1,25 @@
-import { Button, Container, Heading, Stack, Textarea } from "@chakra-ui/react";
+import { Button, Container, Heading, VStack, HStack, Textarea, Select } from "@chakra-ui/react";
 import DataTable, { TableData } from "./DataTable";
 
 import { extractCsvStrings } from "../utils/extractionUtils";
 import { usePapaParse } from "react-papaparse";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CsvParser() {
   const { readString } = usePapaParse();
   const [userInput, setUserInput] = useState<string>("");
   const [tables, setTables] = useState<TableData[]>([]);
+  const [delimiter, setDelimiter] = useState<string>("");
+  const tab = "\t";
+
+  useEffect(() => {
+    console.log(delimiter)
+  },[delimiter])
 
   const handleParseCSV = () => {
     setTables([]);
 
-    const tableList = extractCsvStrings(userInput, ",", 1);
+    const tableList = extractCsvStrings(userInput, delimiter, 1);
 
     tableList.forEach((tableString) => {
       readString(tableString, {
@@ -26,7 +32,15 @@ export default function CsvParser() {
 
   return (
     <Container maxW="4xl" p="1rem">
-      <Stack spacing="1rem">
+      <VStack spacing="1rem">
+        <HStack>
+          <label>Delimiter: </label>
+          <Select defaultValue={[","]} name="delimiter" id="delimiter" value={delimiter} onChange={(e) => setDelimiter(e.target.value)}>
+            <option value=",">CSV (,)</option>
+            <option value={tab}>TSV ("  ")</option>
+            <option value="custom">Custom</option>
+          </Select>
+        </HStack>
         <Textarea
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
@@ -48,7 +62,7 @@ export default function CsvParser() {
             <DataTable key={index} data={table} />
           </>
         ))}
-      </Stack>
+      </VStack>
     </Container>
   );
 }
